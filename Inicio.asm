cc1ea0976cb678476731876ccb8b2f91c06a748b
wozgeass <adrian@aztli.org>
2016-03-09 22:36:53 -0600

    Agregue el archivo a modificar

---
+++ Inicio.asm
@@ -0,0 +1,39 @@
+; Bootloader piñata
+
+BITS 16
+
+start:
+	mov ax, 07C0h   
+	add ax, 20h     
+	mov ss, ax     
+	mov sp, 4096  
+
+	mov ax, 07C0h  
+	mov ds, ax     
+
+	mov si, texto 
+	call imprimir   
+	cli            
+	hlt           
+
+data:
+	texto db 'Hola mundo Especializacion en Linux!', 0
+
+imprimir:
+	mov ah, 0Eh   
+	               
+.imprime_caracter:
+	lodsb          
+	cmp al, 0
+	je .realizado  
+	int 10h        
+	jmp .imprime_caracter 
+.realizado:
+	ret
+
+times 510-($-$$) db 0
+dw 0xAA55	      
+
+
+
+
